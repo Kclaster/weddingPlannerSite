66c0b68b2a3fb820b0934d9d069401490d870720
Kyle Laster <kyle.laster@gmail.com>
2019-02-18 15:42:07 -0600

    Updates

---
+++ precache-manifest.2c1b354750001e09a387e8454dd44202.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/weddingPlannerSite/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "8d763df505d1365032ea",
+    "url": "/weddingPlannerSite/static/js/runtime~main.8d763df5.js"
+  },
+  {
+    "revision": "67faf43ee5f0ce84e24b",
+    "url": "/weddingPlannerSite/static/js/main.67faf43e.chunk.js"
+  },
+  {
+    "revision": "ba1ca5ba3f9c1e3c91ad",
+    "url": "/weddingPlannerSite/static/js/2.ba1ca5ba.chunk.js"
+  },
+  {
+    "revision": "67faf43ee5f0ce84e24b",
+    "url": "/weddingPlannerSite/static/css/main.1b7cfdee.chunk.css"
+  },
+  {
+    "revision": "8e2794190aca0a120da27ac2fdc5f8c0",
+    "url": "/weddingPlannerSite/index.html"
+  }
+];
