c0c2cb09e81f4479d271b29269b79563aae7ef0c
Kyle Laster <kyle.laster@gmail.com>
2019-05-26 17:10:13 -0500

    fort images styled correctly

--- src/components/mainpages/HomePage.js
+++ src/components/mainpages/HomePage.js
@@ -6,11 +6,21 @@ import '../../styles/Body.css';
 import Header from '../Header';
 import ScrollingPics from '../ScrollingPics';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 
 import alyssaFourteen from '../../pics/Alyssa and Ryne/fourteen.jpg';
 import { setHeight } from '../../actions';
 import four from '../../pics/Karley and Chris/four.jpg';
 import one from '../../pics/Karley and Chris/one.jpg';
+
+const StyledImage = styled.img`
+  width: 100%;
+`;
+
+const ImageWrapper = styled.div`
+  width: 30vw;
+  padding: 0 2vw;
+`;
 
 class Body extends React.Component {
   render() {
@@ -20,32 +30,32 @@ class Body extends React.Component {
         <ScrollingPics />
         <div className="body-container">
           <div className="homeImageContainer">
-            <div className="scalingImage-container">
+            <ImageWrapper className="scalingImage-container">
               <h2>Service</h2>
               <Link className="scalingImage" to="/Services">
-                <img
+                <StyledImage
                   className="scale"
                   src={alyssaFourteen}
                   alt="celebration line"
                 />
               </Link>
-            </div>
-            <div className="scalingImage-container">
+            </ImageWrapper>
+            <ImageWrapper className="scalingImage-container">
               <h2>Contact Me</h2>
               <Link className="scalingImage" to="/Contact">
-                <img className="scale" src={one} alt="gazeebo" />
+                <StyledImage className="scale" src={one} alt="gazeebo" />
               </Link>
-            </div>
-            <div className="scalingImage-container">
+            </ImageWrapper>
+            <ImageWrapper className="scalingImage-container">
               <h2>Stories</h2>
               <Link className="scalingImage" to="/Weddings">
-                <img
+                <StyledImage
                   className="scale"
                   src={four}
                   alt="couple holding eachother"
                 />
               </Link>
-            </div>
+            </ImageWrapper>
           </div>
           <Footer />
         </div>
